1716ca6c668b29513d85d49ef7ea7ed88da4d0b3
Mat Aldao <mataldao@Mats-MBP.fibertel.com.ar>
2023-12-14 12:10:14 -0300

    edit google gtm

--- js/main.js
+++ js/main.js
@@ -460,6 +460,7 @@ barba.init({
 });
 
 function googleAnalytics() {
+  dataLayer.push({event: 'pageview', page_url : pageName})
   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
